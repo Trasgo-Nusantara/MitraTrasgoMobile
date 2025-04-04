ddbbe6f8f4191b76563c671ec147302e87d03e7d
hilmanzu <hilmanzutech@gmail.com>
2025-04-04 22:08:15 +0700

    feat:

--- src/screen/feature/trasride/ChatScreen.jsx
+++ src/screen/feature/trasride/ChatScreen.jsx
@@ -1,33 +1,55 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { View, StyleSheet, Dimensions, StatusBar, ScrollView, Text, TextInput, TouchableOpacity } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { COLORS, COMPONENT_STYLES } from '../../../lib/constants';
+import { postData } from '../../../api/service';
 
 const { width } = Dimensions.get('window');
 
-const ChatScreen = ({route}) => {
-  const {idDriver,idOrder,idUser} = route.params
+const ChatScreen = ({ route }) => {
+  const { idDriver, idOrder, idUser } = route.params
   const [message, setMessage] = useState(''); // Untuk mengontrol input pesan
   const [messages, setMessages] = useState([
     // { id: 1, text: 'Halo! Titik jemput sesuai aplikasi ?', sender: 'other' },
   ]);
-  
+
   // Referensi ScrollView untuk memanipulasi scroll
   const scrollViewRef = useRef();
 
   // Fungsi untuk menangani pengiriman pesan
-  const sendMessage = () => {
+  const sendMessage = async () => {
     if (message.trim() !== '') {
       const newMessage = { id: messages.length + 1, text: message, sender: 'user' };
       setMessages([...messages, newMessage]);
-      setMessage(''); // Menghapus input pesan setelah dikirim
+      setMessage('');
+      const form = {
+        idDriver: idDriver,
+        idOrder: idOrder,
+        idUser: idUser,
+        message: message,
+      }
+      await postData(`Chat/sendWA`, form );
     }
   };
+
+  const getProfileUser = useCallback(async () => {
+    const form = {
+      idDriver: idDriver,
+      idOrder: idOrder,
+      idUser: idUser
+    }
+    try {
+      const response = await postData(`Chat/getWA`, form);
+      setMessages(response.message.data)
+    } catch (error) {
+
+    }
+  }, []);
 
   // Gunakan useEffect untuk scroll otomatis ke bawah setiap kali ada perubahan pada pesan
   useEffect(() => {
     scrollViewRef.current?.scrollToEnd({ animated: true });
-    console.log(idDriver,idOrder,idUser)
+    getProfileUser();
   }, [messages]); // Akan dijalankan setiap kali pesan berubah
 
   return (
@@ -40,9 +62,9 @@ const ChatScreen = ({route}) => {
         {messages.map((msg) => (
           <View
             key={msg.id}
-            style={[styles.messageContainer, msg.sender === 'user' ? styles.userMessage : styles.otherMessage]}
+            style={[styles.messageContainer, msg.sender === 'Mitra' ? styles.userMessage : styles.otherMessage]}
           >
-            <Text style={[styles.messageText]}>{msg.text}</Text>
+            <Text style={[styles.messageText]}>{msg.message}</Text>
           </View>
         ))}
       </ScrollView>
@@ -57,7 +79,7 @@ const ChatScreen = ({route}) => {
           placeholderTextColor="gray"
         />
         <TouchableOpacity style={styles.sendButton} onPress={sendMessage}>
-          <Ionicons name="send" size={24} color={COLORS.white} />
+          <Ionicons name="send" size={24} color={COLORS.background} />
         </TouchableOpacity>
       </View>
     </View>
@@ -82,11 +104,11 @@ const styles = StyleSheet.create({
   },
   otherMessage: {
     alignSelf: 'flex-start',
-    backgroundColor: COLORS.lightGray,
+    backgroundColor: "black",
   },
   messageText: {
     fontSize: 16,
-    color: COLORS.white,
+    color: COLORS.background,
   },
   inputContainer: {
     flexDirection: 'row',
